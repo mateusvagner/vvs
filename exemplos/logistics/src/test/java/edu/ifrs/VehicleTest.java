8e00738eab1d7bbcda2fc3b02ea5fa716be9f0b5
Mateus Vagner <mateus.vagner@gmail.com>
2023-09-24 20:58:55 -0300

    test: add more test cases

--- exemplos/logistics/src/test/java/edu/ifrs/VehicleTest.java
+++ exemplos/logistics/src/test/java/edu/ifrs/VehicleTest.java
@@ -12,10 +12,10 @@ public class VehicleTest {
 
     private Vehicle vehicle;
 
+    private final int maxWeightLimit = 1000;
+
     @Before
     public void setUp() {
-        // Initialize a new Vehicle object with a weight limit of 1000
-        int maxWeightLimit = 1000;
         vehicle = new Vehicle(maxWeightLimit);
     }
 
@@ -26,11 +26,12 @@ public class VehicleTest {
 
         // Check the load is added
         assertEquals(1, vehicle.getLoads().size());
+        assertEquals(500, vehicle.getLoads().get(0).getWeight());
     }
 
     @Test
-    public void testCheckWeightLimitExceeded() {
-        // Add a load of weight 800 to the vehicle
+    public void testCheckWeightLimitExceededWithOneLoad() {
+        // Add a load of weight 1500 to the vehicle
         vehicle.addWeight(new Load(1500));
 
         // Check that the weight limit is exceeded
@@ -38,9 +39,39 @@ public class VehicleTest {
     }
 
     @Test
-    public void testCheckWeightLimitNotExceeded() {
+    public void testCheckWeightLimitExceededWithManyLoads() {
+        // Add a load of weight 1500 to the vehicle
+        vehicle.addWeight(new Load(500));
+        vehicle.addWeight(new Load(500));
+        vehicle.addWeight(new Load(500));
+
+        // Check that the weight limit is exceeded
+        assertFalse(vehicle.checkWeightLimit());
+    }
+
+    @Test
+    public void testCheckWeightLimitNotExceededWithOneLoad() {
         // Add a load of weight 500 to the vehicle
         vehicle.addWeight(new Load(500));
+
+        // Check that the weight limit is not exceeded
+        assertTrue(vehicle.checkWeightLimit());
+    }
+
+    @Test
+    public void testCheckWeightLimitNotExceededWithManyLoads() {
+        // Add a load of weight 750 to the vehicle
+        vehicle.addWeight(new Load(500));
+        vehicle.addWeight(new Load(250));
+
+        // Check that the weight limit is not exceeded
+        assertTrue(vehicle.checkWeightLimit());
+    }
+
+    @Test
+    public void testCheckWeightLimitNotExceededWithExactlyMaxWeight() {
+        // Add a load of weight maxWeightLimit to the vehicle
+        vehicle.addWeight(new Load(maxWeightLimit));
 
         // Check that the weight limit is not exceeded
         assertTrue(vehicle.checkWeightLimit());
